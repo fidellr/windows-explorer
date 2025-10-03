260b286caaff820cc0be040020cc24f06317314a
Fidel R <fidelramadhan@gmail.com>
2025-10-03 12:57:47 +0700

    fix: update db connection config

--- explorer-backend/src/infrastructure/config/database.ts
+++ explorer-backend/src/infrastructure/config/database.ts
@@ -3,7 +3,7 @@ import { Client } from "pg";
 // DB connection
 const DATABASE_URL =
   process.env.DATABASE_URL ||
-  "postgres://fidelramadhan:science97@localhost:5432/win_explorer_db";
+  "postgres://postgres:postgres@localhost:5432/win_explorer_db";
 
 export const pg = new Client({ connectionString: DATABASE_URL });
 await pg.connect();
